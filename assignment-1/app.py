e27e1b692dde03c09d3ea298f4214d448f0b1796
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-01-12 14:20:42 +0500

    update assignments

---
+++ assignment-1/app.py
@@ -0,0 +1,16 @@
+per = 40
+
+if per >= 80:
+    print("You got A1 grade")
+elif per >= 70 and per < 80:
+    print("You got A grade")
+elif per >= 60 and per < 70:
+    print("You got B grade")
+elif per >= 50 and per < 60:
+    print("You got C grade")
+elif per >= 40 and per < 50:
+    print("You got D grade")
+elif per >= 33 and per < 40:
+    print("You got E grade")
+else:
+    print("You are Fail")
---
+++ assignment-2/app.py
@@ -0,0 +1,4 @@
+# Table of 2
+
+for i in range(1, 11):
+    print("2 x", i,"=", i* 2)
